4107d5e4af1703509199baee270e71503f26d3c2
Sagang <sagangwee@yahoo.com>
2016-06-21 16:27:05 -0700

    Added custom scroll bar jquery plugin

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -2,9 +2,26 @@ $(function() {
 	var $chatBox = $('#messages');
 	$('#chat').on('keyup', '#message_body', function(e) {
 		if (e.which == 13) {
-			$chatBox.animate({ scrollTop: $chatBox.prop('scrollHeight')}, 1000);
+			// $chatBox.animate({ scrollTop: $chatBox.prop('scrollHeight')}, 1000);
+			$('.custom-scroll').mCustomScrollbar({
+			    theme:"minimal-dark"
+			});
+			$chatBox.mCustomScrollbar("scrollTo", 'bottom', {
+				scrollInertia: 1000
+			});
 	    }
 	});
 
-	$chatBox.prop({ scrollTop: $chatBox.prop("scrollHeight") });
+	// $chatBox.prop({ scrollTop: $chatBox.prop("scrollHeight") });
+
+	// initialize scrollbars
+	$('.custom-scroll').mCustomScrollbar({
+	    theme:"minimal-dark"
+	});
+
+	// focus on bottom messages in chat box
+	$chatBox.mCustomScrollbar("scrollTo", 'bottom', {
+		scrollInertia: 0
+	});
+
 });
