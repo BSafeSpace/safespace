03a8e34d7e0f48871c77d8c5759cde866cce6d71
Mason Scott <mscott2757@berkeley.edu>
2016-06-24 16:34:04 -0700

    added a display for the number of users found

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,6 +7,7 @@ class ProfilesController < ApplicationController
     @search = Profile.search(params[:q])
     @search.build_sort if @search.sorts.empty?
     @profiles = @search.result(distinct: true)
+    @profiles = @profiles.reject { |p| p.user == current_user} if current_user
     gon.profiles = @profiles
     # @profiles = Profile.all
 
@@ -57,6 +58,7 @@ class ProfilesController < ApplicationController
     @search = Profile.search(params[:q])
     @search.build_sort if @search.sorts.empty?
     @profiles = @search.result(distinct: true)
+    @profiles = @profiles.reject { |p| p.user == current_user} if current_user
     gon.profiles = @profiles
     # @profiles = Profile.all
 
