5d82961589537ae412d30e85578a2defd3771b66
Sagang <sagangwee@yahoo.com>
2016-06-27 15:46:26 -0700

    Highlight on hover for nav links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,8 @@ module ApplicationHelper
 
     def link_to_in_li(body, url, html_options = {})
 	  active = "active" if current_page?(url)
-	  content_tag :li, class: active do
+	  puts "active"
+	  content_tag :li, class: "#{active} current" do
 	    link_to body, url, html_options
 	  end
 	end
