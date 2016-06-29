22be38ec4f40b254349587650843d077810b0f57
Sagang <sagangwee@yahoo.com>
2016-06-28 19:58:04 -0700

    committing routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :conversations do
     get 'download_chat', on: :member
     post 'mute', on: :member
+    delete 'mute', on: :member
     resources :messages
   end
 
