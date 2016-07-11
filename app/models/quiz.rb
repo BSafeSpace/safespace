32dfc826d27d59428ca97f78bd7a2eae362e5e98
Sagang <sagangwee@yahoo.com>
2016-07-11 14:47:56 -0700

    Fixed bug with quiz not accepting correct answer

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,8 +1,8 @@
 class Quiz < ActiveRecord::Base
 	belongs_to :user
-	validates :answer1, presence: true, acceptance: { accept: [true, 'TRUE', 'correct'], message: 'Incorrect answer' }
-	validates :answer2, presence: true, acceptance: { accept: ['TRUE', 'correct'], message: 'Incorrect answer' }
-	validates :answer3, presence: true, acceptance: { accept: ['TRUE', 'correct'], message: 'Incorrect answer' }
-	validates :answer4, presence: true, acceptance: { accept: ['TRUE', 'correct'], message: 'Incorrect answer' }
-	validates :answer5, presence: true, acceptance: { accept: ['TRUE', 'correct'], message: 'Incorrect answer' }
+	validates :answer1, presence: true, inclusion: { in: %w(correct), message: 'Incorrect answer' }
+	validates :answer2, presence: true, inclusion: { in: %w(correct), message: 'Incorrect answer' }
+	validates :answer3, presence: true, inclusion: { in: %w(correct), message: 'Incorrect answer' }
+	validates :answer4, presence: true, inclusion: { in: %w(correct), message: 'Incorrect answer' }
+	validates :answer5, presence: true, inclusion: { in: %w(correct), message: 'Incorrect answer' }
 end
