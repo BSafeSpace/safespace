b65b52948786eaf52be8935f1a31b2370bfd5fdd
Sagang <sagangwee@yahoo.com>
2016-07-15 00:49:58 -0700

    Added Chat liability

--- app/assets/javascripts/conversations.js
+++ app/assets/javascripts/conversations.js
@@ -14,4 +14,28 @@ $('.conversations.index').ready(function() {
 		$('#new_recommendation').submit();
 	});
 
+	$( "#dialog-confirm" ).dialog({
+		buttons: {
+			"I agree": function() {
+				$( this ).dialog( "close" );
+			},
+			Cancel: function() {
+				window.location.replace("http://www.webtechnologyinc.com");
+			}
+		},
+		closeOnEscape: false,
+		draggable: false,
+		height: ($(window).height()) * 0.8,
+		modal: true,
+		open: function(event, ui) {
+			// Set opacity of parent window
+			$( ".ui-widget-overlay" ).css( "opacity", 0.90 );
+			$( ".ui-widget-overlay" ).css( "z-index", 0 );
+			// Hide close button on modal dialog
+			$( ".ui-dialog-titlebar-close" ).css( "display", 'none' );
+		},
+		resizable: false,
+		width: ($(window).width()) * 0.7
+		});
+
 });
