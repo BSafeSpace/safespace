b72331abefb39b5da1755c29ea98ef8f154c1d7d
Sagang <sagangwee@yahoo.com>
2016-07-15 18:27:55 -0700

    Create profile for users in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,7 @@ questions = {mental_health: [omit_message, "No issue: Seeking out peers", "Depre
 			class_standing: [omit_message, "Freshman", "Sophomore", "Junior", "Senior", "Other"],
 			gender: [omit_message, "Male", "Female", "Transgender", "Intersex", "Gender Queer", "Gender Nonconforming", "Questioning", "Other"],
 			ethnicity: [omit_message, "African/African American", "Alaskan Native", "Asian", "Chinese/Chinese American", "Filipino/Filipino American", "Vietnamese/Vietnamese American", "Mexican/Mexican American/Chicano", "Native American", "East Indian or Pakistani","Japanese/Japanese American", "Korean/Korean American","Pacific Islander","Middle Eastern","White/Caucasian", "Hispanic/Latino", "Other"],
-			academic_focus: [omit_message, "Undecided","Arts & Humanities","Biological Sciences","Business","Design","Economic Development & Sustainability", "Education","Engineering & Computer Science","Mathematics","Multi-Disciplinary","Natural Resources & Environment", "Physical Sciences","Pre-Health/Medicine","Pre-Law","Social Sciences","Other"],
+			academic_focus: [omit_message, "Undecided","Arts & Humanities","Biological Sciences","Business","Design","Economic Development & Sustainability", "Education","Engineering & Computer Science","Mathematics","Multi-Disciplinary","Natural Resources & Environment", "Physical Sciences", "Pre-Health/Medicine","Pre-Law","Social Sciences","Other"],
 			socioeconomic_status: [omit_message, "Of low socioeconomic status", "Of middle socioeconomic status", "Of high socioeconomic status", "Other"],
 			sexual_orientation: [omit_message, "Heterosexual", "Gay", "Bisexual", "Questioning", "Fluid", "Queer", "Asexual", "Other"],
 			religion: [omit_message, "Baha’i","Buddhism","Christianity","Hinduism","Islam","Jainism","Judaism","Shinto","Sikhism","Taoism","Atheist", "Agnostic","Other"]}
@@ -39,6 +39,7 @@ questions.each do |category, names|
 end
 
 User.all.each do |u|
+	u.create_profile
 	u.profile.age = rand(16...60)
 	u.profile.save
 end
