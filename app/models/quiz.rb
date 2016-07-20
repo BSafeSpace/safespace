d235341a01d01ad7587f05f464297db11adae309
Sagang <sagangwee@yahoo.com>
2016-07-20 01:27:07 -0700

    Updated Quiz content

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,8 +1,7 @@
 class Quiz < ActiveRecord::Base
 	belongs_to :user
-	validates :answer1, presence: true, inclusion: { in: %w(correct), message: 'incorrect answer' }
-	validates :answer2, presence: true, inclusion: { in: %w(correct), message: 'incorrect answer' }
-	validates :answer3, presence: true, inclusion: { in: %w(correct), message: 'incorrect answer' }
-	validates :answer4, presence: true, inclusion: { in: %w(correct), message: 'incorrect answer' }
-	validates :answer5, presence: true, inclusion: { in: %w(correct), message: 'incorrect answer' }
+	validates :answer1, presence: true, inclusion: { in: %w(Brainstorm\ Solutions\ and\ Don’t\ Judge), message: 'incorrect answer' }
+	validates :answer2, presence: true, inclusion: { in: %w(Block\ a\ student:\ If\ a\ student\ is\ blocked\ 3\ times,\ they\ are\ permanently\ deleted\ from\ SafeSpace.), message: 'incorrect answer' }
+	validates :answer3, presence: true, inclusion: { in: %w(True), message: 'incorrect answer' }
 end
+	
