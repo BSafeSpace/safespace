b3e26b683d07c53b53182ded10c6eeec2255e07c
Sagang <sagangwee@yahoo.com>
2016-07-21 06:51:43 -0700

    Trying info logging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,10 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  # config.log_level = :debug
+
+  # Log detail is configurable on the server
+  config.log_level = ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].to_sym : ('info').to_sym
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
