4651d9273250c5d951aa4622c5608e01a4fe5077
Sagang <sagangwee@yahoo.com>
2016-07-22 06:18:15 -0700

    Basic resource page

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,13 +1,29 @@
 class ResourcesController < ApplicationController
   def index
+  	@resources = Resource.all
+  end
+
+  def new
+  	@resource = Resource.new
   end
 
   def create
+  	@resource = Resource.create! resource_params
+  	respond_to do |format|
+  		format.html
+  		format.js
+  	end
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+
+  def resource_params
+	params.require(:resource).permit(:name, :description, :url)
   end
 end
--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -1,2 +1,5 @@
 module ResourcesHelper
+	def url_with_protocol(url)
+	    /^http/i.match(url) ? url : "http://#{url}"
+	end
 end
