afb65f9f2796d68a1eeea8fc96bdfa9a9187a5a9
Sagang <sagangwee@yahoo.com>
2016-07-23 21:33:26 -0700

    Can now re-arrange resources

--- app/assets/javascripts/resources.js
+++ app/assets/javascripts/resources.js
@@ -5,4 +5,39 @@ $(function() {
 		$resourceItem.show();
 		console.log('clicked');
 	});
+
+	$(".resources").sortable({
+	    placeholder: 'resource-placeholder',
+	    axis: "y",
+	    revert: 150,
+	    start: function(e, ui){
+	        
+	        placeholderHeight = ui.item.outerHeight();
+	        ui.placeholder.height(placeholderHeight + 15);
+	        $('<div class="resource-placeholder-animator" data-height="' + placeholderHeight + '"></div>').insertAfter(ui.placeholder);
+	    
+	    },
+	    change: function(event, ui) {
+	        
+	        ui.placeholder.stop().height(0).animate({
+	            height: ui.item.outerHeight() + 15
+	        }, 300);
+	        
+	        placeholderAnimatorHeight = parseInt($(".resource-placeholder-animator").attr("data-height"));
+	        
+	        $(".resource-placeholder-animator").stop().height(placeholderAnimatorHeight + 15).animate({
+	            height: 0
+	        }, 300, function() {
+	            $(this).remove();
+	            placeholderHeight = ui.item.outerHeight();
+	            $('<div class="resource-placeholder-animator" data-height="' + placeholderHeight + '"></div>').insertAfter(ui.placeholder);
+	        });
+	        
+	    },
+	    stop: function(e, ui) {
+	        
+	        $(".resource-placeholder-animator").remove();
+	        
+	    },
+	});
 });
