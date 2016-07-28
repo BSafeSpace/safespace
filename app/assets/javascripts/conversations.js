a853f23ae46521baa4fce994ae9923050aac1c7a
Sagang <sagangwee@yahoo.com>
2016-07-28 02:21:34 -0700

    Protocol link copy

--- app/assets/javascripts/conversations.js
+++ app/assets/javascripts/conversations.js
@@ -44,14 +44,21 @@ $('.conversations.index').ready(function() {
 	$(".ui-dialog-buttonset button:contains('I agree')").addClass('btn').addClass('btn-success');
 	$(".ui-dialog-buttonset button:contains('Cancel')").addClass('btn').addClass('btn-default');
 
-	// Add Protocol button
+	// Protocol Button 
 	var $protocolContainer = $('<div class="protocol-container"></div>');
 	$protocolContainer.appendTo($("body"));
 	var $protocolButton = $('<div class="protocol-button vertical-text btn btn-danger">PROTOCOL</div>');
-	$protocolButton.appendTo($('.protocol-container'));
+	$protocolButton.appendTo($protocolContainer);
 	var $protocolMenu =  $('<div class="protocol-menu margin-negative"></div>');
 	$protocolMenu.appendTo($('.protocol-container'));
+	$protocolMenu.html('<h4>You are not here to counsel. Please send your peer this link.</h4>\
+						<div class="copy-container input-group">\
+							<input class="input-form-url" id="suicide-input" type="text" value="http://www.suicidepreventionlifeline.org/" />\
+							<div class="copy-button btn btn-success btn-lg">Copy</div>\
+						</div>'
+						);
 
+	// Expand Protocol Menu on Protocol Button Click
 	$protocolButton.click(function() {
 		$protocolMenu = $('.protocol-menu');
 		if ($protocolMenu.hasClass('margin-zero')) {
@@ -62,5 +69,82 @@ $('.conversations.index').ready(function() {
 			console.log('second toggle');
 		}
 	});
+
+	// Select input on click
+	$("#suicide-input").click(function () {
+	   $(this).select();
+	});
+
+	// Copy link on copy button click
+	$('.copy-button').click(function() {
+	    copyToClipboardMsg(document.getElementById("suicide-input"), "msg");
+	});
+
+	function copyToClipboardMsg(elem, msgElem) {
+		var succeed = copyToClipboard(elem);
+	    var msg;
+	    if (!succeed) {
+	        msg = "Copy not supported or blocked.  Press Ctrl+c to copy."
+	    } else {
+	        msg = "Text copied to the clipboard."
+	    }
+	    if (typeof msgElem === "string") {
+	        msgElem = document.getElementById(msgElem);
+	    }
+	    msgElem.innerHTML = msg;
+	    setTimeout(function() {
+	        msgElem.innerHTML = "";
+	    }, 2000);
+	}
+
+	function copyToClipboard(elem) {
+		  // create hidden text element, if it doesn't already exist
+	    var targetId = "_hiddenCopyText_";
+	    var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
+	    var origSelectionStart, origSelectionEnd;
+	    if (isInput) {
+	        // can just use the original source element for the selection and copy
+	        target = elem;
+	        origSelectionStart = elem.selectionStart;
+	        origSelectionEnd = elem.selectionEnd;
+	    } else {
+	        // must use a temporary form element for the selection and copy
+	        target = document.getElementById(targetId);
+	        if (!target) {
+	            var target = document.createElement("textarea");
+	            target.style.position = "absolute";
+	            target.style.left = "-9999px";
+	            target.style.top = "0";
+	            target.id = targetId;
+	            document.body.appendChild(target);
+	        }
+	        target.textContent = elem.textContent;
+	    }
+	    // select the content
+	    var currentFocus = document.activeElement;
+	    target.focus();
+	    target.setSelectionRange(0, target.value.length);
+	    
+	    // copy the selection
+	    var succeed;
+	    try {
+	    	  succeed = document.execCommand("copy");
+	    } catch(e) {
+	        succeed = false;
+	    }
+	    // restore original focus
+	    if (currentFocus && typeof currentFocus.focus === "function") {
+	        currentFocus.focus();
+	    }
+	    
+	    if (isInput) {
+	        // restore prior selection
+	        elem.setSelectionRange(origSelectionStart, origSelectionEnd);
+	    } else {
+	        // clear temporary content
+	        target.textContent = "";
+	    }
+	    return succeed;
+	}
 
 });
