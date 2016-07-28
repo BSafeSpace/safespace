61f33349cfbd68b25d38cc9e0b35b52f2aab7c00
Sagang <sagangwee@yahoo.com>
2016-07-28 03:57:26 -0700

    Styled and animated flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -55,4 +55,13 @@ module ApplicationHelper
       @devise_mapping ||= Devise.mappings[:user]
     end
 
+    def flash_class(level)
+	    case level.to_sym
+	    when :notice then "alert alert-info"
+	    when :success then "alert alert-success"
+	    when :error then "alert alert-error"
+	    when :alert then "alert alert-error"
+	    end
+	end
+
 end
