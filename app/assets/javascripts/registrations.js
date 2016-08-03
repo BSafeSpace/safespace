b47f6a9632af9de22876066391727cb1e1a86599
Sagang <sagangwee@yahoo.com>
2016-08-03 16:40:06 -0700

    Updated email tooltip

--- app/assets/javascripts/registrations.js
+++ app/assets/javascripts/registrations.js
@@ -5,7 +5,7 @@ $(function() {
 	  html: true,
 	  trigger: 'hover',
 	  template: '<div class="tooltip dropdown-tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>',
-	  title: 'Why are we asking for your email on an anonymous site? We want to make sure that only Berkeley students are allowed to join.',
+	  title: 'Why are we asking for your email on an anonymous site? We want to make sure that only Berkeley students are allowed to join. However, <strong>your identity and information will be kept anonymous.</strong>',
 	  placement: 'right'
 	});
 
