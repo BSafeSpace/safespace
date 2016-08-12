1e1d70ba34549ebe1c84d3fc4477791c86b3350a
Sagang <sagangwee@yahoo.com>
2016-08-11 19:13:11 -0700

    Updated incorrect quiz answer message

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,8 +1,8 @@
 class Quiz < ActiveRecord::Base
 	belongs_to :user
-	validates :answer1, presence: true, inclusion: { in: %w(Brainstorm\ Solutions\ and\ Don’t\ Judge), message: 'incorrect answer' }
-	validates :answer2, presence: true, inclusion: { in: %w(Block\ a\ student:\ If\ a\ student\ is\ blocked\ 3\ times,\ they\ are\ permanently\ deleted\ from\ SafeSpace.), message: 'incorrect answer' }
-	validates :answer3, presence: true, inclusion: { in: %w(True), message: 'incorrect answer' }
-	validates :answer4, presence: true, inclusion: { in: %w(True), message: 'incorrect answer' }
+	validates :answer1, presence: true, inclusion: { in: %w(Brainstorm\ Solutions\ and\ Don’t\ Judge), message: 'Try again' }
+	validates :answer2, presence: true, inclusion: { in: %w(Block\ a\ student:\ If\ a\ student\ is\ blocked\ 3\ times,\ they\ are\ permanently\ deleted\ from\ SafeSpace.), message: 'Try again' }
+	validates :answer3, presence: true, inclusion: { in: %w(True), message: 'Try again' }
+	validates :answer4, presence: true, inclusion: { in: %w(True), message: 'Try again' }
 end
 	
