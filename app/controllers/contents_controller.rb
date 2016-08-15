2306773996a34f13b6758c81c050fc9737f523a0
Mason Scott <mscott2757@berkeley.edu>
2016-08-15 01:34:51 -0700

    can now edit the slides..

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,2 +1,26 @@
 class ContentsController < ApplicationController
+	before_action :set_content
+
+	def set_content
+		@content = Content.first
+	end
+
+	def index
+
+	end
+
+	def update
+		@content = Content.first
+		@content.update(content_params)
+		@content_tag = params[:content_tag]
+		@content_name = params[:content_name]
+
+		respond_to do |format|
+			format.html { redirect_to :back }
+		end
+	end
+
+	def content_params
+		params.require(:content).permit(:slide_one, :slide_two, :slide_three, :slide_four, :terms_of_service)
+	end
 end
