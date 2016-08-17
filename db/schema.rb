b7c10522710d6bb0de8e76775ed2605f19f61b82
Sagang <sagangwee@yahoo.com>
2016-08-16 22:20:47 -0700

    Ran migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817051027) do
+ActiveRecord::Schema.define(version: 20160817051713) do
 
   create_table "blocks", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -177,6 +177,7 @@ ActiveRecord::Schema.define(version: 20160817051027) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
