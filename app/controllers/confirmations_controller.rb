0eb06cde28820d42e534c35a2aa9d300fa9c3f74
Sagang <sagangwee@yahoo.com>
2016-08-29 22:13:07 -0700

    Sign in user after confirming email

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -3,6 +3,7 @@ class ConfirmationsController < Devise::ConfirmationsController
   private
 
   def after_confirmation_path_for(resource_name, resource)
+  	sign_in(resource)
     intro_info_path
   end
 
