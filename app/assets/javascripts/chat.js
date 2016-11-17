e5714df6babf9f8a57018ab41b08137e16bcc625
Kim Wang <gz2ca@berkeley.edu>
2016-11-17 00:41:41 -0800

    removed Pending/Requests on chat page

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -2,7 +2,7 @@ $(function() {
 	var $chatBox = $('#messages');
 	$('#chat').on('keyup', '#message_body', function(e) {
 		if (e.which == 13) {
-			// $chatBox.animate({ scrollTop: $chatBox.prop('scrollHeight')}, 1000);
+			$chatBox.animate({ scrollTop: $chatBox.prop('scrollHeight')}, 1000);
 	    }
 	});
 
