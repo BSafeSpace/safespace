cd7e0b292b053a8246c3a18d069fb8fc078c6a69
jacobavelino1 <jacob7227@yahoo.com>
2018-01-31 23:07:04 -0800

    Removed old gmail mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,15 +22,15 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default :charset => "utf-8"
 
-  config.action_mailer.smtp_settings = {
-    address: "localhost",
-    port: 1025,
-    domain: ENV["GMAIL_DOMAIN"],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["safespacebear@gmail.com"],
-    password: ENV["timetotalk2017"]
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: "localhost",
+  #   port: 1025,
+  #   domain: ENV["GMAIL_DOMAIN"],
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: ENV["GMAIL_USERNAME"],
+  #   password: ENV["GMAIL_PASSWORD"]
+  # }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
