6eed100df146fa869e9a7a4145a0e570c64b07d4
jacobavelino1 <jacob7227@yahoo.com>
2018-02-07 22:34:31 -0800

    Deleted migration AddConfirmableToDevise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817220953) do
+ActiveRecord::Schema.define(version: 20180208055812) do
 
   create_table "blocks", force: :cascade do |t|
     t.datetime "created_at", null: false
