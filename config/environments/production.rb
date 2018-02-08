e32700ee57b61b412b2db368d1c7405381c8b0e4
jacobavelino1 <jacob7227@yahoo.com>
2018-02-07 23:14:48 -0800

    Changed from dev to test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,9 +63,9 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
-  config.action_mailer.default_url_options = { :host => 'safespace-dev.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'safespace-test.herokuapp.com' }
 
-  Rails.application.routes.default_url_options[:host] = 'safespace-dev.herokuapp.com'
+  Rails.application.routes.default_url_options[:host] = 'safespace-test.herokuapp.com'
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
