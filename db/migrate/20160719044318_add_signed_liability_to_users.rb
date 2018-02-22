9c82e1dce90e585fbb3a113d22676bf53748b977
Wilson Wang <wilsonw926@gmail.com>
2018-02-21 21:20:21 -0800

    signed liability migration

--- db/migrate/20160719044318_add_signed_liability_to_users.rb
+++ db/migrate/20160719044318_add_signed_liability_to_users.rb
@@ -1,5 +1,5 @@
 class AddSignedLiabilityToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :signed_liability, :boolean, default: false
+    add_column :users, :signed_liability, :boolean, default: true
   end
 end
